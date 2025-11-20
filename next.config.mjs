685f291fa9d6cd9e68bdb4530fd11fdec0aee390
Lev <lc@lev.la>
2025-11-20 10:53:04 -0800

    fix images

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ import remarkGfm from 'remark-gfm'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true,
+  },
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
